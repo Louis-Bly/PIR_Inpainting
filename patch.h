4381d86c57fdeb9b89fd79a43ef47dcaa3323cc0
Ludovic-DANIS <ludovic.danis@eleves.enpc.fr>
2021-05-20 18:21:01 +0200

    Fonction trouvant le meilleur patch

--- patch.h
+++ patch.h
@@ -5,14 +5,20 @@
 #include <Imagine/Graphics.h>
 
 int const N=9; //N DOIT être impair
+const float INF = 1.0f/0.0; // Infini en float
 
 typedef Image<Color> Img;
 
 class patch{
     pixel p;
-    int colors[N*N][3]; //3 -> rgb
+    int colors[N*N][3]; //Couleurs RGB(3) pour chacun des N*N pixels du patch
 public:
-    patch();
-    patch(int x, int y, Img img);
-    patch remplacant(pixel p, Img img);
+    patch(); // Constructeur vide initialisant toutes les variables a 0
+    patch(int x, int y, Img img); // Patch ayant pour centre le pixel (x,y)
+    int getx();
+    int gety();
+    bool inclu(int C[], int W); // Renvoi vrai si tous les pixels sont dans l'image source, et non dans l'image vide a completer
+    float distance(patch pat, int C[], int W); // calcul la distance euclidienne RGB entre les deux patchs
+                                               // "pat" est le patch remplacant
+    patch remplacant(pixel pix, int C[], int W, int H, Img img);  // Renvoie le meilleur patch associe au patch centre en p
 };
